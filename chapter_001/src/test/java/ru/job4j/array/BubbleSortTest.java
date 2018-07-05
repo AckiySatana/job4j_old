8a64d0be06038afc07762b04d85cd888e566cda3
Ackiy Satana <mailto:NaDerevnu@dedushke.ru>
2018-07-05 22:32:35 +0300

    6.5. Сорировка Пузырьком

--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -12,7 +12,6 @@ public class BubbleSortTest {
         BubbleSort bubba = new BubbleSort();
         int[]  result, a =  {7,6,4,8,9,5};
         result = bubba.sort(a);
-
         assertThat(result,is(a));
     }
 
