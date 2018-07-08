4d6bdeb681037b1acec61ab4af59c4abe228d79d
Ackiy Satana <mailto:NaDerevnu@dedushke.ru>
2018-07-08 19:37:25 +0300

    6.7. Квадратный массив

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
-
         boolean result = true;
-        int len = Math.min(data.length, data[0].length);
-        for (int i = 0; i < len; i++) {
-            if ((data[0][0] != data[i][i]) || (data[0][0] != data[len - 1 - i][i])) {
+        for (int i = 0; i < data.length; i++) {
+            if ((data[0][0] != data[i][i]) || (data[data.length - 1][0] != data[data.length - 1 - i][i])) {
                 result = false;
                 break;
             }
