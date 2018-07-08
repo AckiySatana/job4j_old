7dea8d5d0e36cad039292cb350a140251dbc3b8c
Ackiy Satana <mailto:NaDerevnu@dedushke.ru>
2018-07-08 13:28:00 +0300

    6.7. Квадратный массив

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,8 +5,6 @@ public class MatrixCheck {
 
         boolean result = true;
         int len = Math.min(data.length, data[0].length);
-        System.out.println();
-
         for (int i = 0; i < len; i++) {
             if ((data[0][0] != data[i][i]) || (data[0][0] != data[len - 1 - i][i])) {
                 result = false;
